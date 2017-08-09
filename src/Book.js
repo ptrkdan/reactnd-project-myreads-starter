d4d29106d6c2c1b4d1ed361f41df68233d2f1bc7
Patrick Dan Lacuna <me@ptrklacuna.com>
2017-08-09 15:18:34 +0900

    fix: Display blank book image if thumbnail is not available
    Fix: #6

--- src/Book.js
+++ src/Book.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 
 function Book(props) {
   const { book, updateBook } = props;
+  const thumbnail = book.imageLinks && book.imageLinks.thumbnail;
 
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})`}}></div>
         <div className="book-shelf-changer">
           <select value="default"
             onChange={ event => {
