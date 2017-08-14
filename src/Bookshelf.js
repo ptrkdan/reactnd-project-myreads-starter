0bc855b2f9b4dc1e80c5c88defa6a4a76aa24c78
Patrick Dan Lacuna <me@ptrklacuna.com>
2017-08-14 17:24:12 +0900

    refactor: Change ListBooks and Bookshelf component to functional stateless

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
-class Bookshelf extends Component {
-  static propTypes = {
-    bookshelfTitle: PropTypes.string.isRequired,
-    bookList: PropTypes.array.isRequired,
-    updateBook: PropTypes.func.isRequired
-  }
-  render() {
-    let { bookshelfTitle, bookList, updateBook } = this.props;
-    
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{ bookshelfTitle }</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            { 
-              bookList[0] && (
-                bookList.map( (book) => ( 
-                  <li key={book.id}>
-                    <Book book={book} updateBook={updateBook} />
-                  </li>
-                ))
-              )
-            }
-          </ol>
-        </div>
+function Bookshelf(props) {
+  const { bookshelfTitle, bookList, updateBook } = props;
+
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{ bookshelfTitle }</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          { 
+            bookList[0] && (
+              bookList.map( (book) => ( 
+                <li key={book.id}>
+                  <Book book={book} updateBook={updateBook} />
+                </li>
+              ))
+            )
+          }
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+Bookshelf.propTypes = {
+  bookshelfTitle: PropTypes.string.isRequired,
+  bookList: PropTypes.array.isRequired,
+  updateBook: PropTypes.func.isRequired
 }
 
 export default Bookshelf;
